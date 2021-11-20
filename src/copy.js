8bc4e43f3297cf4f6958c8bc19f72c613036cb39
Frank Pagan <frank@cocreate.app>
2021-11-20 18:41:05 +0000

    feat: copy.js to copy inner or outterhtml and add to cipboard

---
+++ src/copy.js
@@ -0,0 +1,57 @@
+/*global CustomEvent*/
+import action from '@cocreate/action';
+import toolbar from '@cocreate/toolbar';
+import text from '@cocreate/text';
+
+function  cloneElement(btn) {
+	let element = btn.closest('toolbar, .toolbar');
+	let targetElement = element.toolbar.target;
+	let domTextEditor = targetElement.ownerDocument.documentElement;
+	
+	var clone = targetElement.cloneNode(true);
+
+	text.insertAdjacentElement({
+		domTextEditor,
+		position: 'afterend',
+		target: targetElement,
+		elementValue: clone.outerHTML,
+	});
+	
+	dispatchClickEvent(clone);
+}
+
+function  dispatchClickEvent(target) {
+    let clickEvent = new CustomEvent('click', { bubbles: true });
+    Object.defineProperty(clickEvent, 'target', { writable: false, value: target });
+    target.ownerDocument.dispatchEvent(clickEvent);
+}
+
+function  deleteElement(btn) {
+	let element = btn.closest('toolbar, .toolbar');
+	let targetElement = element.toolbar.target;
+	let domTextEditor = targetElement.ownerDocument.documentElement;
+
+	text.removeElement({
+		domTextEditor,
+		target: targetElement
+	});
+	
+	toolbar.hide(element);
+}
+
+action.init({
+	action: "deleteElement",
+	endEvent: "deleteElement",
+	callback: (btn, data) => {
+		deleteElement(btn);
+	}
+});
+
+action.init({
+	action: "cloneElement",
+	endEvent: "cloneElement",
+	callback: (btn, data) => {
+		cloneElement(btn);
+	}
+});
+
