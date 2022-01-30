089403f890fc23c45091ffd887093ae193dba831
Fran Pagan <frank@cocreate.app>
2022-01-30 03:23:23 +0000

    fix: removed deleteElement. Was replaced by CoCreate-remove

--- src/cloneElement.js
+++ src/cloneElement.js
@@ -1,6 +1,5 @@
 /*global CustomEvent*/
 import action from '@cocreate/actions';
-import toolbar from '@cocreate/toolbar';
 import text from '@cocreate/text';
 
 function  cloneElement(btn) {
@@ -25,27 +24,6 @@ function  dispatchClickEvent(target) {
     Object.defineProperty(clickEvent, 'target', { writable: false, value: target });
     target.ownerDocument.dispatchEvent(clickEvent);
 }
-
-function  deleteElement(btn) {
-	let element = btn.closest('toolbar, .toolbar');
-	let targetElement = element.toolbar.target;
-	let domTextEditor = targetElement.ownerDocument.documentElement;
-
-	text.removeElement({
-		domTextEditor,
-		target: targetElement
-	});
-	
-	toolbar.hide(element);
-}
-
-action.init({
-	action: "deleteElement",
-	endEvent: "deleteElement",
-	callback: (btn, data) => {
-		deleteElement(btn);
-	}
-});
 
 action.init({
 	action: "cloneElement",
